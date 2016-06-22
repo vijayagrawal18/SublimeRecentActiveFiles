bd9c9f2963c7d83b055be43a218429135ad02263
Vijay Kumar Agrawal <vijay@bigbinary.com>
2016-06-22 00:29:52 -0400

    Created File class 2. Fixed folder bug

--- RecentActiveFiles.py
+++ RecentActiveFiles.py
@@ -9,33 +9,46 @@ class RecentActiveFilesEventListener(sublime_plugin.EventListener):
 class RecentActiveFilesCommand(sublime_plugin.WindowCommand):
     def __init__(self, window):
         sublime_plugin.WindowCommand.__init__(self, window)
-        self.recent_active_files = []
-
-    def unshift(self, file_name):
-        if file_name in self.recent_active_files:
-            self.recent_active_files.remove(file_name)
-        self.recent_active_files.insert(0, file_name)
-
-    def path_form_project(self, path):
-        for folder in self.window.folders():
-            path = path.replace(folder + '/', '', 1)
-        return path
+        File.project_folder = self.window.folders()[0]
 
     def run(self, file_name=None):
         if file_name:
-            self.unshift(file_name)
+            File.add(file_name)
         else:
-            items = [[os.path.basename(f), self.path_form_project(f)] for f in self.recent_active_files]
-
+            items = [[f.name(), f.path_from_project()] for f in File.all_recents]
             def on_done(index):
-                if index >= 0:
-                    self.window.open_file(items[index][1])
-                else:
-                    if len(self.recent_active_files) > 0:
-                        self.window.open_file(items[0][1])
+                if index < 0:
+                    self.window.open_file(File.current_file.path)
 
             def on_highlight(index):
                 if index >= 0:
-                    self.window.open_file(items[index][1], sublime.TRANSIENT)
+                    self.window.open_file(File.all_recents[index].path, sublime.TRANSIENT)
+                return True
 
-            self.window.show_quick_panel(items, on_done, sublime.MONOSPACE_FONT, -1, on_highlight)
+            self.window.show_quick_panel(items, on_done, 0, -1, on_highlight)
+
+class File(object):
+    all_recents = []
+    current_file = None
+    project_folder = None
+
+    def __init__(self, path):
+        super(File, self).__init__()
+        self.path = path
+
+    def __eq__(self, other):
+        return isinstance(other, File) and self.path == other.path
+
+    @classmethod
+    def add(cls, path):
+        if cls.current_file is not None:
+            cls.all_recents.insert(0, cls.current_file)
+        cls.current_file = cls(path)
+        if cls.current_file in cls.all_recents:
+            cls.all_recents.remove(cls.current_file)
+
+    def name(self):
+        return os.path.basename(self.path)
+
+    def path_from_project(self):
+        return self.path.replace(self.project_folder + '/', '', 1)
